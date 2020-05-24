eb11af793b63430b6538338be74edb3665c27d2a
bal3000 <bal3000286@googlemail.com>
2020-05-24 23:36:07 +0100

    state set up for form

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CoursesPage(): JSX.Element {
-  return <h2>Courses</h2>;
+  // TODO: Add <Course>
+  const [course, setCourse] = useState({ title: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setCourse({ ...course, title: event.target.value });
+
+  return (
+    <form>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type="text" onChange={handleChange} value={course.title} />
+      <input type="submit" value="Save" />
+    </form>
+  );
 }
 
 export default CoursesPage;
